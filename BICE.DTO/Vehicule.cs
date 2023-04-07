2cf7997b6f30d9899e7b8aae2d2ba93215dd357b
Saayli <hessabilyas@gmail.com>
2023-04-07 16:45:44 +0200

    Création des DTO

---
+++ BICE.DTO/Vehicule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BICE.DTO
+{
+    internal class Vehicule
+    {
+        public int Id { get; set; }
+        public string Immatriculation { get; set; }
+        public string denomination { get; set; }
+        public string Numero { get; set; }
+    }
+}
